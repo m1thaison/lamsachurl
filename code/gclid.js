652a64a377589a8481e2288c997ed3fc6433ad21
m1thaison <121505002+m1thaison@users.noreply.github.com>
2022-12-27 22:18:33 +0700

    Add files via upload

---
+++ code/gclid.js
@@ -0,0 +1,6 @@
+// Lam sach URL: https://lamsachurl.thaison.site
+// Chia se boi Lam Thai Son: https://thaison.page
+
+// Remove gclid
+(function(){var param='gclid';if(location.search.indexOf(param+'=')!==-1){var replace='';try{var url=new URL(location);url.searchParams.delete(param);replace=url.href}catch(ex){var regExp=new RegExp('[?&]'+param+'=.*$');replace=location.search.replace(regExp,'');replace=location.pathname+replace+location.hash}
+history.replaceState(null,'',replace)}})();
