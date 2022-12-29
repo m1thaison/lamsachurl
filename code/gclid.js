fef6c14e986f827b35542d25b20691eafaced75a
m1thaison <121505002+m1thaison@users.noreply.github.com>
2022-12-30 01:13:28 +0700

    Update gclid.js

--- code/gclid.js
+++ code/gclid.js
@@ -1,5 +1,5 @@
-// Lam sach URL: https://lamsachurl.thaison.site
-// Chia se boi Lam Thai Son: https://thaison.page
+// Lam sach URL: https://lamsachurl.thaison.page
+// Chia se boi Lam Thai Son: https://thaison.site
 
 // Remove gclid
 (function(){var param='gclid';if(location.search.indexOf(param+'=')!==-1){var replace='';try{var url=new URL(location);url.searchParams.delete(param);replace=url.href}catch(ex){var regExp=new RegExp('[?&]'+param+'=.*$');replace=location.search.replace(regExp,'');replace=location.pathname+replace+location.hash}
