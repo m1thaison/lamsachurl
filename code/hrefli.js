a2ee2f2dedfb4b7f7041a959495fc520a98a70f2
m1thaison <121505002+m1thaison@users.noreply.github.com>
2022-12-30 01:13:50 +0700

    Update hrefli.js

--- code/hrefli.js
+++ code/hrefli.js
@@ -1,5 +1,5 @@
-// Lam sach URL: https://lamsachurl.thaison.site
-// Chia se boi Lam Thai Son: https://thaison.page
+// Lam sach URL: https://lamsachurl.thaison.page
+// Chia se boi Lam Thai Son: https://thaison.site
 
 // Remove href.li
 function RemoveHrefLi(){var e=document.querySelectorAll('a[href*="href.li/?"]');Array.prototype.forEach.call(e,(function(r,t){var f=e[t].getAttribute("href").split("href.li/?")[1];e[t].setAttribute("href",f)}))}RemoveHrefLi();
